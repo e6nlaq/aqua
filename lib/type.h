1a427e26ec59751a8ed5b77b5598a316ed3653d8
e6nlaq <imcadajp@gmail.com120777733+e6nlaq@users.noreply.github.com>
2023-02-25 13:15:19 +0900

    Added the greatest common divisor function `gcd` and the least common multiple function `lcm` Fixes #264

---
+++ lib/type.h
@@ -0,0 +1,21 @@
+
+/*******************************************************
+
+              lib/type.h
+
+    Created at: Feb 25, 2023, 11:38 AM GMT+9
+    Copyright (C) 2023 e6nlaq
+    This file is part of aqua.
+    Licensed under the MIT License.
+
+*******************************************************/
+
+#pragma once
+
+#include <vector>
+
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using vll = std::vector<ll>;
