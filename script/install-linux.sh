5d1fd1ea6e4fc7ca66f0a6677901b9aabe082a04
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-03-11 18:17:12 +0900

    Add installation files for linux Fixes #300

---
+++ script/install-linux.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+# This file is part of aqua.
+# Licensed under the MIT License.
+
+now=`pwd`
+mkdir temp
+cd temp
+tag="1.5.0-pre2"
+wget https://github.com/e6nlaq/aqua/archive/refs/tags/v$tag.tar.gz
+tar -zxvf v$tag.tar.gz
+cd ./aqua-$tag/bin/x64/
+cp ./aqua ~/.local/bin
+cd ~/.local/bin
+chmod +x ./aqua
+
+cd $now
+rm -rf ./temp/
